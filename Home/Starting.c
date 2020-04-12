67a1ae73ebb6f5cdb7c00a43fe50ba3b3c7034f8
iBehzod <alimov.behzod@gmail.com>
2020-04-12 12:26:02 +0500

    Initial Commit

--- Home/Starting.c
+++ Home/Starting.c
@@ -5,8 +5,12 @@ int main() {
     int a;
     printf("Hello world again,meet me again %d", max_max);
     scanf("%d", &a);
+    
+    
     printf("Hello again");
+    
     printf("''__''");
+    
     return 0;
 
 }
