e36d7365bd06da6eef2a0090574f6c2642a53bba
Behzod <alimov.behzod@gmail.com>
2020-04-24 02:08:17 +0500

    Initial Commit

--- Apps.py
+++ Apps.py
@@ -27,7 +27,7 @@
 #
 ##
 ###
-
+####
 
                 
 
@@ -36,6 +36,8 @@
 # num1 = float(input("Enter first number : "))
 # op = input("Enter operator: ")
 # num2 = float(input("Enter second number : "))
+
+
 
 # if op == "+":
 #     print(num1 + num2)
