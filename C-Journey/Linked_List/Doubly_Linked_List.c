db99b682f9beee2c100b56b271ecf3765c80b101
iBehzod <33046317+iBehzod@users.noreply.github.com>
2020-06-06 16:21:09 +0500

    Upgrade Doubly_Linked_List.c

--- C-Journey/Linked_List/Doubly_Linked_List.c
+++ C-Journey/Linked_List/Doubly_Linked_List.c
@@ -53,17 +53,32 @@ int Length(struct Node *p)
 void Insert(struct Node *p, int index, int x)
 {
     struct Node *t;
-    int x, i;
-    if(index < 0 || index < Length(p))  // for checking the given number is valid or not
+    int i;
+    if(index < 0 || index > Length(p))  // for checking the given number is valid or not
         return;
     if (index  ==0)
     {
+     t = (struct Node *)malloc(sizeof(struct Node));
      t->data = x;
      t->prev = NULL;
      t->next = first;
      first->prev = t;
      first = t;    
     }
+    else
+    {
+        
+        for(i=0; i<index-1; i++)
+            p = p->next;
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = x;
+        
+        t->next = p->next;
+        t->prev = p;
+        if(p->next!=NULL)p->next->prev = t; // checks whether the Node is last or not, if it last it will 
+        p->next = t; 						// use 3 pointers, otherwise 4;
+    }
+    
 }
 
 int main()
@@ -73,7 +88,11 @@ int main()
 
     printf("\nLength is: %d \n",  Length(first));
     
+    
+    Insert(first, 0, 99);
+    
     Display(first);
+
 
     return 0;
 }
