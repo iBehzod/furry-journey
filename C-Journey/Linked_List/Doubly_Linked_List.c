67036737147461a171956feff55033b76125d70d
iBehzod <33046317+iBehzod@users.noreply.github.com>
2020-06-06 11:48:02 +0500

    Upgrade

---
+++ C-Journey/Linked_List/Doubly_Linked_List.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node 
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+}*first = NULL;
+
+void create(int A[], int n)
+{
+    struct Node *t, *last;
+    int i;
+
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->prev = first->next = NULL; // to make both pointers NULL {[NULL/prev][data][NULL/next]}
+    last = first;
+    for(i=1; i<n; i++)
+    {
+        t= (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = last->next;
+        t->prev = last;
+        last->next = t;
+        last = t; 
+    }
+}  
+
+void Display(struct Node *p)
+{
+    while (p)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+int Length(struct Node *p)
+{
+    int len =0;
+    while (p!=NULL)
+    {
+        len++;
+        p = p->next;
+    }
+    return len;
+}  
+//int argc, char const *argv[] 
+int main()
+{
+    int A[] = {5, 10, 20, 30 , 40};
+    create(A, 5);
+
+    printf("\nLength is: %d \n", Length(first));
+    
+    Display(first);
+
+    return 0;
+}
