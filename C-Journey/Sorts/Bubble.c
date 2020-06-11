9c08fecc0e197b50accd0450ce00948a82c697fd
iBehzod <33046317+iBehzod@users.noreply.github.com>
2020-06-11 10:07:46 +0500

    Initial Commit

--- C-Journey/Sorts/Bubble.c
+++ C-Journey/Sorts/Bubble.c
@@ -1,7 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-
+//#include<stdlib.h>
 void swap(int *x, int *y)
 {
     int temp=*x;
@@ -11,8 +9,7 @@ void swap(int *x, int *y)
 
 void Bubble(int A[], int n)
 {
-    int i,j;
-    int flag=0;
+    int i,j,flag=0;
 
     for(i=0; i<n-1; i++)
     {
@@ -31,14 +28,63 @@ void Bubble(int A[], int n)
         
 }
 
+
+
 int main()
 {
-    int A[]= {5,3,2,2,9,11,31,29,86,10};
-    Bubble(A, 10);
-    int i;
-
+    int A[]= {5,3,2,2,9,11,31,19,39,1},n=10,i;
+    
+    Bubble(A, n);
+    
     for(i=0; i<10; i++)
         printf("%d ", A[i]);
     printf("\n");
+    
     return 0;
 }
+//          //          //          //
+
+//          //          //          // 
+
+//          //          //          //
+// #include <stdio.h>
+// #include<stdlib.h>
+// void swap(int *x,int *y)
+// {
+//     int temp=*x;
+//     *x=*y;
+//     *y=temp;
+// }
+// void Bubble(int A[],int n)
+// {
+//     int i,j,flag=0;
+    
+//     for(i=0;i<n-1;i++)
+//     {
+//         flag=0;
+//         for(j=0;j<n-i-1;j++)
+//         {
+//             if(A[j]>A[j+1])
+//             {
+//                 swap(&A[j],&A[j+1]);
+//                 flag=1;
+//             }
+//         }
+//         if(flag==0)
+//             break;
+//     }
+    
+// }
+// int main()
+// {
+//    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
+    
+//     Bubble(A,n);
+    
+//     for(i=0;i<10;i++)
+//         printf("%d ",A[i]);
+//     printf("\n");
+    
+//     return 0;
+// }
+
