5889831c6af3c494afe87cda270e3e380a5557ec
Bekhzod <alimov.behzod@gmail.com>
2020-06-13 09:34:41 +0500

    Initial Commit

--- C-Lab/2-BinarySearch.c
+++ C-Lab/2-BinarySearch.c
@@ -1,61 +1,40 @@
+// Write a program to search an element using Binary Search. 
+#include<stdio.h>
 
-// #include <stdio.h>
-// int main()
-// {
-//    int c, first, last, middle, n, search, array[100];
-//    printf("Enter number of elements:\n");
-//    scanf("%d",&n); 
-//    printf("Enter %d integers:\n", n);
-//    for (c = 0; c < n; c++)
-//       scanf("%d",&array[c]); 
-//    printf("Enter the value to find:\n");
-//    scanf("%d", &search);
-//    first = 0;
-//    last = n - 1;
-//    middle = (first+last)/2;
-//    while (first <= last) {
-//       if (array[middle] < search)
-//          first = middle + 1;    
-//       else if (array[middle] == search) {
-//          printf("%d is present at index %d.\n", search, middle+1);
-//          break;
-//       }
-//       else
-//          last = middle - 1;
-//       middle = (first + last)/2;
-//    }
-//    if (first > last)
-//       printf("Not found! %d is not present in the list.\n", search);
-//    return 0;  
-// }
+int main()
+{
+   int c, l, h, mid, n, search, arr[100];
 
-#include <stdio.h>
-#include<stdint.h>
+   printf("Enter the amount of elements: \n");
+   scanf("%d", &n);
 
-int binarySearch(int arr[], int l, int r, int x) 
-{ 
-    if (r >= l) { 
-        int mid = l + (r - l) / 2; 
-        if (arr[mid] == x) 
-            return mid; 
-        if (arr[mid] > x) 
-            return binarySearch(arr, l, mid - 1, x); 
-        return binarySearch(arr, mid + 1, r, x); 
-    } 
-    return -1; 
-}  
-int main(void) 
-{ 
-    int arr[] = {5, 10, 23, 24 ,30, 40, 100 }; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int x = 10; 
-    int result = binarySearch(arr, 0, n - 1, x); 
-    (result == -1) ? printf("\nThe element is not present in array\n") 
-                   : printf("\nThe element is present at index %d\n", 
-                            result); 
-    int i;n=7;
-    for(i=0; i<n; i++)
-        printf("%d \n", arr[i]);
-    printf("\n");
-    return 0; 
+   printf("Enter %d integer(s): \n", n);
+   for(c=0;c<n;c++)
+      scanf("%d", &arr[c]);
+   
+   printf("Enter the value to find: \n");
+   scanf("%d", &search);
+
+   l = 0;
+   h = n-1;
+   mid = (l + h)/2;
+
+   while (l <= h)
+   {
+      if(arr[mid] < search)
+         l = mid +1;
+      else if(arr[mid] == search){
+         printf("%d is found at index %d", search, mid +1);
+         break;}
+      else
+         h  = mid -1;
+         mid =  (l + h)/2;
+   }
+
+   if(h<l)
+      printf("Value %d is not found! Try once more.", search);
+
+   return 0;
+   
+   
 }
