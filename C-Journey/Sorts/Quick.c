27a034407ff3e6560e8596e2354ce6363e2280d9
Bekhzod <alimov.behzod@gmail.com>
2020-06-13 10:53:12 +0500

    Initial Commit

--- C-Journey/Sorts/Quick.c
+++ C-Journey/Sorts/Quick.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
+
+                                          
 void swap(int *x,int *y)
 {
     int temp=*x;
--- C-Lab/6-QuickSort.c
+++ C-Lab/6-QuickSort.c
@@ -1,1 +1,25 @@
 #include<stdio.h>
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int partition(int A[], int l, int h)
+{
+    
+}
+
+
+int main()
+{
+    int A[]={11,13,7,12,16,9,24,5,10,3},n=10,i;
+    
+    QuickSort(A,0,n);
+    
+    for(i=0;i<10;i++)
+        printf("%d ",A[i]);
+    printf("\n");
+
+    return 0;
+}
